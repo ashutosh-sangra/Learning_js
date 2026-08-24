9ecca720fa248f66150316f2098e618ed156bfc0
Ashutosh Sangra <ashutoshsangra78@gmail.com>
2026-08-25 03:52:45 +0530

    changes

--- 3.Loops/14.numofdigits.js
+++ 3.Loops/14.numofdigits.js
@@ -1,12 +1,13 @@
-let num = 12345; // change this to whatever number you want
+let num = 1234589; // change this to whatever number you want
 let count = 0;
 let temp = num;
 
-if (temp === 0) {
+if (num === 0) {
   count = 1; // special case: 0 has 1 digit
 } else {
-  temp = Math.abs(temp); // handle negative numbers
+  //   temp = Math.abs(temp); // handle negative numbers
   while (temp > 0) {
+    let lastd = temp % 10;
     count++;
     temp = Math.floor(temp / 10);
   }
--- 3.Loops/15.sumofdigitsofnumber.js
+++ 3.Loops/15.sumofdigitsofnumber.js
@@ -1,11 +1,11 @@
-let num = 1234; // change this to whatever number you want
+let num = 12345; // change this to whatever number you want
 let sum = 0;
 let temp = num;
 
 while (temp > 0) {
   let lastDigit = temp % 10;
   sum += lastDigit;
-  temp = (temp - lastDigit) / 10; // integer division without Math.floor
+  temp = Math.floor(temp / 10); // integer division without Math.floor //temp-lastdigit//
 }
 
 console.log("Sum of digits: " + sum);
